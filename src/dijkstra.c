921de13761d598337fabcf545e8055454435f6d3
Kristiyan Peychev <kurostuki_l@yahoo.com>
2014-09-18 05:35:55 +0300

    Moved the main function to a separate file

--- src/dijkstra.c
+++ src/dijkstra.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "graph.h"
 
-long int get_min_dist_node(long int *Q, unsigned long int *dist, unsigned sz) 
+static long int get_min_dist_node(long int *Q, unsigned long int *dist, unsigned sz) 
 { /* We're actually looking for the index of the node in Q */
 	long int p = -1, i = 0;
 	unsigned long int g = (unsigned long int) -1l;
@@ -58,45 +58,7 @@ long int *dijkstra(graph *G, node *s)
 return prev;
 }
 
-int main(int argc, const char *argv[]) {
-	graph *G = graph_init();
-	node **ns;
-	unsigned N = 0, M = 0, cc = 0, wbuf, nbuf, mbuf;
-	long int *path;
-	scanf("%u %u", &N, &M);
-	ns = (node **) malloc(N * sizeof(node *));
-	while (cc < N) {
-		ns[cc] = node_init(cc);
-		cc++;
-	}
-	cc = 0;
-	while (cc < M) {
-		scanf("%u %u %u", &nbuf, &mbuf, &wbuf);
-		node_add_edge(ns[nbuf], ns[mbuf], wbuf);
-		cc++;
-	}
-	scanf("%u %u", &nbuf, &mbuf);
-	cc = 0;
-	while (cc < N) {
-		graph_add_node(G, ns[cc]);
-		cc++;
-	}
-	/* CALL DIJKSTRA SOMEHOW */
-	path = dijkstra(G, G->nodes[nbuf]);
-	cc = 0;
-	while (path[mbuf] != -1l && path[mbuf] != -1l && path[mbuf] != 0) {
-		cc++;
-		mbuf = path[mbuf];
-	}
-	if (path[mbuf] != -1l) {
-		printf("%d\n", cc);
-	} else {
-		printf("-1\n");
-	}
-return 0;
-}
-
-#if 0
+#ifdef _TEST_DIJKSTRA_ 
 int main(int argc, const char *argv[]) {
 	unsigned long int *f;
 	graph *G = graph_init();
